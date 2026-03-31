46c8edff46f42db43d6b8f916ca7a1bb6bd0e3c6
Tom Planche <tomplanche@proton.me>
2026-03-31 12:43:07 +0200

    [17] (fix on main) Fix website info fetching

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,4 @@
-import { WEBSITE_INFOS_URL } from '$env/static/private';
+import commits from '../../static/website_infos.json';
 
 type CommitInfo = {
 	sha: string;
@@ -6,36 +6,16 @@ type CommitInfo = {
 	url: string;
 };
 
-export const load = async (): Promise<{ lastCommit: CommitInfo | null }> => {
-	try {
-		const response = await fetch(WEBSITE_INFOS_URL);
+export const load = (): { lastCommit: CommitInfo | null } => {
+	const commit = commits[0];
 
-		if (!response.ok) {
-			console.error(response.statusText);
+	if (!commit) return { lastCommit: null };
 
-			return { lastCommit: null };
+	return {
+		lastCommit: {
+			sha: commit.sha.slice(0, 7),
+			date: commit.commit.committer.date,
+			url: commit.html_url
 		}
-
-		const commits = (await response.json()) as Array<{
-			sha: string;
-			html_url: string;
-			commit: {
-				committer: { date: string };
-			};
-		}>;
-
-		const commit = commits[0];
-
-		return {
-			lastCommit: {
-				sha: commit.sha.slice(0, 7),
-				date: commit.commit.committer.date,
-				url: commit.html_url
-			}
-		};
-	} catch (error) {
-		console.error('Error fetching last commit:', error);
-
-		return { lastCommit: null };
-	}
+	};
 };
